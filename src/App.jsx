bd121a7b585bf37aa3d3709f852faf1976e8b09d
Yottabyte-tech <callanchem314@gmail.com>
2025-08-15 15:19:21 +0000

    Added Operations

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,24 @@ function App() {
 
   return (
     <>
+    <div class='FullEquation'>
+      <div class='FractionFull'>
       <div class='fraction' id='Numerator'>
           <input class='fractionNumber' type='number' style={{ width: '50px', height: '50px'}}></input>
         </div>
       <p></p>
       <div class='fraction' id='Denominator'>
           <input class='fractionNumber' type='number' style={{ width: '50px', height: '50px'}}></input>
-        </div>                
+        </div>     
+      </div>
+          <select class='Operation' id="Operation" name="Select Operation" style={{ width: '50px', height: '50px', textAlign: 'center', fontSize: '35px'}}>
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>    
+
+          </div>       
     </>
   )
 }
