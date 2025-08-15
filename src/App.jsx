41589b17ec32f95c9bc171d3eaaff185dbd127a3
Yottabyte-tech <callanchem314@gmail.com>
2025-08-15 15:05:42 +0000

    Added First Fraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numOne, setNumOne] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div class='fraction' id='Numerator'>
+          <input class='fractionNumber' type='number' style={{ width: '50px', height: '50px'}}></input>
+        </div>
+      <p></p>
+      <div class='fraction' id='Denominator'>
+          <input class='fractionNumber' type='number' style={{ width: '50px', height: '50px'}}></input>
+        </div>                
     </>
   )
 }
